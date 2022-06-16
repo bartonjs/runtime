944ed03b4f5209a84adc404f810ab265b6d1ac01
Jeremy Barton <jbarton@microsoft.com>
2022-06-16 07:37:38 -0700

    Add some tests that build CRLs

--- src/libraries/System.Security.Cryptography.X509Certificates/tests/CertificateCreation/CrlBuilderTests.cs
+++ src/libraries/System.Security.Cryptography.X509Certificates/tests/CertificateCreation/CrlBuilderTests.cs
@@ -408,35 +408,7 @@ namespace System.Security.Cryptography.X509Certificates.Tests.CertificateCreatio
                 }
             }
         }
-
-        private static void BuildCertificateAndRun(
-            IEnumerable<X509Extension> extensions,
-            Action<X509Certificate2, DateTimeOffset> action,
-            [CallerMemberName] string callerName = null)
-        {
-            using (ECDsa key = ECDsa.Create())
-            {
-                CertificateRequest req = new CertificateRequest(
-                    $"CN=\"{callerName}\"",
-                    key,
-                    HashAlgorithmName.SHA384);
-
-                req.CertificateExtensions.Add(new X509SubjectKeyIdentifierExtension(req.PublicKey, false));
-
-                foreach (X509Extension ext in extensions)
-                {
-                    req.CertificateExtensions.Add(ext);
-                }
-
-                DateTimeOffset now = DateTimeOffset.UtcNow;
-
-                using (X509Certificate2 cert = req.CreateSelfSigned(now.AddMonths(-1), now.AddMonths(1)))
-                {
-                    action(cert, now);
-                }
-            }
-        }
-
+        
         [Fact]
         public static void BuildWithGeneratorArgumentValidation()
         {
@@ -488,6 +460,93 @@ namespace System.Security.Cryptography.X509Certificates.Tests.CertificateCreatio
         }
 
         [Fact]
+        public static void BuildEmpty()
+        {
+            BuildRsaCertificateAndRun(
+                new X509Extension[]
+                {
+                    X509BasicConstraintsExtension.CreateForCertificateAuthority(),
+                },
+                static (cert, notNow) =>
+                {
+                    HashAlgorithmName hashAlg = HashAlgorithmName.SHA256;
+                    RSASignaturePadding pad = RSASignaturePadding.Pkcs1;
+                    CertificateRevocationListBuilder builder = new CertificateRevocationListBuilder();
+                    DateTimeOffset now = new DateTimeOffset(2013, 4, 6, 7, 58, 9, TimeSpan.Zero);
+
+                    byte[] built = builder.Build(cert, 123, now.AddMinutes(5), now, hashAlg, pad);
+
+                    // The length of the output depends on a number of factors, but they're all stable
+                    // for this test (since it doesn't use ECDSA's variable-length, non-deterministic, signature)
+                    //
+                    // In fact, because RSASSA-PKCS1 is a deterministic algorithm, we can check it for a fixed output.
+
+                    byte[] expected = (
+                        "3082018E3078020101300D06092A864886F70D01010B05003015311330110603" +
+                        "550403130A4275696C64456D707479170D3133303430363037353830395A170D" +
+                        "3133303430363038303330395AA02F302D301F0603551D2304183016801478A5" +
+                        "C75D51667331D5A96924114C9B5FA00D7BCB300A0603551D14040302017B300D" +
+                        "06092A864886F70D01010B0500038201010005A249671E2EE8780CBE17180493" +
+                        "094A5EB576465A9B2674666B762184AD992832556B36CC8320264FE45A6B4981" +
+                        "439ED9CFB87EAD10D4A95769713A0442B2D3A5FD20487DA5B33BCFBE10ED921C" +
+                        "8B9896B69EA443D8D9F0AF5E0EB789361655C80EC3C7C7C84F5127C6A29C27BE" +
+                        "8437CE0182BD16CF697169121C2BBFAADC4EDE17C8BB76949D25376F2739E03C" +
+                        "DA0609D03C024CD5A911B342571F385B3B8A782B62C5375E1D674E43447FE2EB" +
+                        "9EFFCAF71CCCECBAE600C74F6FD6CB36A87C5786603501EA43794144142E8557" +
+                        "EC2EBC2F7357DB050440FD97F233441E2BE981ED6309CE7C8B1C97BCE658FCEC" +
+                        "6BD63004A1D3D4EA0043783E55E7ECBCF6E6").HexToByteArray();
+
+                    Assert.Equal(expected, built);
+                });
+        }
+
+        [Fact]
+        public static void BuildSingleEntry()
+        {
+            BuildRsaCertificateAndRun(
+                new X509Extension[]
+                {
+                    X509BasicConstraintsExtension.CreateForCertificateAuthority(),
+                },
+                static (cert, notNow) =>
+                {
+                    HashAlgorithmName hashAlg = HashAlgorithmName.SHA256;
+                    RSASignaturePadding pad = RSASignaturePadding.Pkcs1;
+                    DateTimeOffset now = new DateTimeOffset(2013, 4, 6, 7, 58, 9, TimeSpan.Zero);
+                    CertificateRevocationListBuilder builder = new CertificateRevocationListBuilder();
+
+                    builder.AddEntry(
+                        stackalloc byte[] { 0x01, 0x01, 0x02, 0x03, 0x05, 0x08, 0x0C, 0x15 },
+                        now.AddSeconds(-1812));
+
+                    byte[] explicitUpdateTime = builder.Build(cert, 123, now.AddMinutes(5), now, hashAlg, pad);
+
+                    // The length of the output depends on a number of factors, but they're all stable
+                    // for this test (since it doesn't use ECDSA's variable-length, non-deterministic, signature)
+                    //
+                    // In fact, because RSASSA-PKCS1 is a deterministic algorithm, we can check it for a fixed output.
+
+                    byte[] expected = (
+                        "308201B230819B020101300D06092A864886F70D01010B0500301B3119301706" +
+                        "0355040313104275696C6453696E676C65456E747279170D3133303430363037" +
+                        "353830395A170D3133303430363038303330395A301B30190208010102030508" +
+                        "0C15170D3133303430363037323735375AA02F302D301F0603551D2304183016" +
+                        "801478A5C75D51667331D5A96924114C9B5FA00D7BCB300A0603551D14040302" +
+                        "017B300D06092A864886F70D01010B05000382010100A9E1D03571B1E4BF7670" +
+                        "EC32459A74B11482741FD973FF5040D57B133B5B6C783DC9ED105C4CF5DDE8FC" +
+                        "8B767C6034253D749A834622034A669AA4C6EFDB93C82EB15B69E6DC43F05BAE" +
+                        "7E9E21B0351A720C5E79F3BE65304658EBDFE196269BC285D653E7ACD97811D6" +
+                        "4E08792034B47D83BF9D37851116023BDF7460C5BF1492CFA486AD7B2F277870" +
+                        "82E6A3C05C0E43BB7D62B234C0E6C5BA2E0103E1CCBDAE15F9CD6DB989DED687" +
+                        "0915AB164EB2FC2ADA00D4980574FC2C3C0905C1BFC9F42DBF0F800FF7F9D92C" +
+                        "1F99C443EFC32593C749E18C41282E0EF232643846D204A6BC23C55605299225" +
+                        "6323F7BD75DEE733C9FD011B6D3B85395422046B5573").HexToByteArray();
+
+                    AssertExtensions.SequenceEqual(expected, explicitUpdateTime);
+                });
+        }
+
+        [Fact]
         public static void BuildSimpleCdp()
         {
             X509Extension ext = CertificateRevocationListBuilder.BuildCrlDistributionPointExtension(
@@ -499,6 +558,63 @@ namespace System.Security.Cryptography.X509Certificates.Tests.CertificateCreatio
                 "6f645369675043415f30382d33312d323031302e63726c").HexToByteArray();
 
             Assert.Equal(expected, ext.RawData);
+        }
+        
+        private static void BuildCertificateAndRun(
+            IEnumerable<X509Extension> extensions,
+            Action<X509Certificate2, DateTimeOffset> action,
+            [CallerMemberName] string callerName = null)
+        {
+            using (ECDsa key = ECDsa.Create())
+            {
+                CertificateRequest req = new CertificateRequest(
+                    $"CN=\"{callerName}\"",
+                    key,
+                    HashAlgorithmName.SHA384);
+
+                req.CertificateExtensions.Add(new X509SubjectKeyIdentifierExtension(req.PublicKey, false));
+
+                foreach (X509Extension ext in extensions)
+                {
+                    req.CertificateExtensions.Add(ext);
+                }
+
+                DateTimeOffset now = DateTimeOffset.UtcNow;
+
+                using (X509Certificate2 cert = req.CreateSelfSigned(now.AddMonths(-1), now.AddMonths(1)))
+                {
+                    action(cert, now);
+                }
+            }
+        }
+
+        private static void BuildRsaCertificateAndRun(
+            IEnumerable<X509Extension> extensions,
+            Action<X509Certificate2, DateTimeOffset> action,
+            [CallerMemberName] string callerName = null)
+        {
+            using (RSA key = RSA.Create(TestData.RsaBigExponentParams))
+            {
+                CertificateRequest req = new CertificateRequest(
+                    $"CN=\"{callerName}\"",
+                    key,
+                    HashAlgorithmName.SHA384,
+                    RSASignaturePadding.Pkcs1);
+
+                req.CertificateExtensions.Add(new X509SubjectKeyIdentifierExtension(req.PublicKey, false));
+
+                foreach (X509Extension ext in extensions)
+                {
+                    req.CertificateExtensions.Add(ext);
+                }
+
+                DateTimeOffset now = DateTimeOffset.UtcNow;
+
+                using (X509Certificate2 cert = req.CreateSelfSigned(now.AddMonths(-1), now.AddMonths(1)))
+                {
+                    action(cert, now);
+                }
+            }
         }
     }
 }
